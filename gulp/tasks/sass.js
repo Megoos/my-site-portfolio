0db242f537be63829f01cff7b2aae6d5af973a39
Megoos <meguskov@gmail.com>
2017-09-20 22:42:33 +0500

    small add-ins

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -2,8 +2,9 @@
 
 module.exports = function () {
     $.gulp.task('sass', function() {
-        return $.gulp.src('./source/style/app.scss')
+        return $.gulp.src('./source/style/app.scss')        
         .pipe($.gp.sourcemaps.init())
+        .pipe($.gp.sassGlob())
         .pipe($.gp.sass())
         .on('error', $.gp.notify.onError({
             title: 'Style'
